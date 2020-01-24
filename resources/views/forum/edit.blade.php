9f3fc09f3a886ed58cb04bdff5806b418385c7b5
alfnonymous8 <58419410+alfnonymous8@users.noreply.github.com>
2020-01-24 23:44:13 +0700

    Design edit page

--- resources/views/forum/edit.blade.php
+++ resources/views/forum/edit.blade.php
@@ -1,7 +1,11 @@
 @extends('layouts.app')
-
+@section('title',"Edit : $forum->title")
 @section('content')
 <div class="container">
+    <div class="jumbotron" id="tc_jumbotron">
+      <div class="col-md-8 offset-md-2">
+        <div class="text-center"><h3 style="color: #fff;">Edit Pertanyaan</h3></div><hr style="background: #fff"> 
+      </div>
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -24,25 +28,45 @@
                                 @endforeach
                             </select>
                         </div>                        
-                        <div class="form-group">
-                            <input type="file" name="image" class="form-control">
-                        </div>
-                        <div class="form-group">
-                            <div class="col-sm-5">
-                                <img src="{{asset('images/' .$forum->image)}}" alt="" width="100%">
-                            </div>
-                        </div>
-                        <button type="submit" class="btn btn-success btn-block">Submit</button>
-                    </form>
+                        <a data-toggle="collapse" data-target="#edit_image"><i class="fa fa-image" id="upload_image"></i></a>
+                    <div id="edit_image" class="collapse">  
+                        <div class="bg">
+                          <div class="form-group">
+                             <input type="file" class="form-control" name="image" placeholder="image" style="background-color: #f5f8fa;"> 
+                                </div>
+                            </div>  
+                            <div class="form-group">
+                               <div class="col-md-4"> 
+                               <img src="{{asset('images/'.$forum->image)}}" alt="" width="100%">
+                               </div>
+                           </div> 
+                          </div>
+                           <br> 
+                      <button type="submit" class="btn btn-success btn-block">Submit</button>
+                   </form>
                 </div>
             </div>
+          <br> 
+        <div class="create_info" style="color: #fff;">
+         <h5><i class="fa fa-info-circle"></i> Info</h5>
+           <span>- Klik icon <strong>Gambar</strong> untuk upload dan edit gambar screenshot.</span> 
+          </div><br>
+          <div class="alert alert-info alert-dismissible fade show" role="alert"> 
+          <b>Pertanyaan terakhir edit:</b><br> 
+           <a href="#" style="color: #444"><h5 style="margin-top: 4px;"><i class="fa fa-newspaper-o"></i> {{$forum->title}}</h5></a>  
+          <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
         </div>
+       </div>
     </div>
+  </div>
 </div>
 @endsection
-
-@section('js')
+  
+ @section('js')
 <script type="text/javascript">
-    $(".tags").select2().val({!! json_encode($forum->tags()->allRelatedIds() ) !!}).trigger('change');
+$(".tags").select2().val({!! json_encode($forum->tags()->allRelatedIds() ) !!}).trigger('change');
+
 </script>
 @endsection

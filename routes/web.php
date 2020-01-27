949a20a875a5483918897dd19efcebfeed72275b
alfnonymous8 <58419410+alfnonymous8@users.noreply.github.com>
2020-01-27 18:00:05 +0700

    Forum slug

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,5 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::resource('/forum', 'ForumController');
+
+Route::get('/forum/read/{slug}', 'ForumController@show')->name('forumslug');

2da7bc37b0e3fbead30894bd0fcac5fe66bb0437
alfnonymous8 <58419410+alfnonymous8@users.noreply.github.com>
2020-01-29 15:42:38 +0700

    Comment progres 2

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -3,8 +3,25 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Forum;
+use App\Comment;
+use Auth;
 
 class CommentController extends Controller
 {
-    //
+    public function addComment(Request $request, Forum $forum)
+    {
+
+        $request->validate([
+            'content' => 'required|min:5'
+        ]);
+
+        $comment = New Comment;
+        $comment->user_id = Auth::user()->id;
+        $comment->content = $request->content;
+
+        $forum->comments()->save($comment);
+
+        return back()->withInfo('Komentar berhasil dikirim!');
+    }
 }

4c6c33ce6bfdb64ef4dfdf9ac3b8fa15b87bca5c
alfnonymous8 <58419410+alfnonymous8@users.noreply.github.com>
2020-02-01 22:18:34 +0700

    User profile done

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -4,11 +4,13 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\User;
+use App\Forum;
 
 class ProfileController extends Controller
 {
     public function index(User $user){
 
-        return view('profile.index', compact('user'));
+        $forums = Forum::where('user_id', $user->id)->get();
+        return view('profile.index', compact('user','forums'));
     }
 }
